3acd0ccec50c615868aaca29f010e74f15ab5ef8
marceloriv <ma.riveracontreras@duocuc.com>
2025-10-13 15:54:01 -0300

    Archivos modificados: Archivos: - App.css     - Cambios solo en el formateo - App.jsx     - Cambio en el formateo y dejarlo limpio - main.jsx Agregados: - components/Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <div className="container position-relative">
+        <h1>PowerFit</h1>
+        <div>
+          <a href="">Paigna</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
