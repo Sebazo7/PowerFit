667bf0f468426624da18fe2f98e2a107a448073e
marceloriv <ma.riveracontreras@duocuc.com>
2025-10-16 21:42:19 -0300

    Corrige la definición de __dirname en vite.config.js para usar path.dirname

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { fileURLToPath } from "url"
 
-const __dirname = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
