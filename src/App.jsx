b20fe74d1ff57a5af5276509c2679d9e9aeba2e9
marceloriv <ma.riveracontreras@duocuc.com>
2025-10-18 17:36:09 -0300

    Envuelve el componente App en BrowserRouter y actualiza la estructura de importación

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
 import Encabezado from "@components/layout/Header"
+import RutasApp from "@/routes/AppRoutes"
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Encabezado />
-    </>
+      <RutasApp />
+    </BrowserRouter>
   )
 }
 
