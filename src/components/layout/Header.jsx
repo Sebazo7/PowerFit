689a3540e3b8f5a9fdda4b56e88979fb2fd4af07
Sebastian N. <marcelonovoanav@gmail.com>
2025-10-19 20:46:39 -0300

    Agrega funcionalidad de carrito en el encabezado y mejora la navegación con enlaces

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,33 @@
-import { Navbar, Container, Nav, Button } from "react-bootstrap"
+import { Navbar, Container, Nav, Button, Badge } from "react-bootstrap"
+import { Link } from 'react-router-dom'
+import { useCart } from '../../contexts/CartContext'
+
 export default function Header() {
+  const { totalItems } = useCart()
   return (
     <>
       <Navbar className="bg-body-tertiary img-fluid">
         <Container>
-          <Navbar.Brand href="#home" className="bad-script-regular">
+          <Navbar.Brand as={Link} to="/" className="bad-script-regular">
             <img
               alt=""
               src="/src/assets/img/logos/logo2.PNG"
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{" "}
+            />{' '}
             PowerFit
           </Navbar.Brand>
-          <Nav.Link href="#home">Inicio</Nav.Link>
-          <Nav.Link href="#productos">Productos</Nav.Link>
-          <Button variant="primary">Iniciar Sesion</Button>
+          <Nav>
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/productos">Productos</Nav.Link>
+          </Nav>
+          <div className="ms-auto d-flex align-items-center">
+            <Button variant="outline-primary" className="me-2">Iniciar Sesion</Button>
+            <Button as={Link} to="/carrito" variant="primary">
+              Carrito {totalItems > 0 && <Badge bg="light" text="dark" className="ms-2">{totalItems}</Badge>}
+            </Button>
+          </div>
         </Container>
       </Navbar>
     </>
