f6b78a8f2593d389570b92067dbcc79c5419f63f
Sebastian N. <marcelonovoanav@gmail.com>
2025-10-19 20:47:21 -0300

    Envuelve el componente App en CartProvider para gestionar el contexto del carrito

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client"
 import "@styles/index.css"
 import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { CartProvider } from './contexts/CartContext'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </StrictMode>
 )
