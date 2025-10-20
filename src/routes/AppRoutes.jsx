f2a5478a3039dc4ea6bdec5aba73fb4beebc6bc9
marceloriv <ma.riveracontreras@duocuc.com>
2025-10-20 05:24:54 -0300

    Eliminar rutas de Home, Products y CatalogWithCart en AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import RutaProtegida from "@/components/RutaProtegida"
 import Inicio from "@/pages/Inicio"
-import Home from "@/pages/Home"
 import Productos from "@/pages/Productos"
-import Products from "@/pages/Products"
-import CatalogWithCart from "@/pages/CatalogWithCart"
 import Marcas from "@/pages/Marcas"
 import Ofertas from "@/pages/Ofertas"
 import Login from "@/pages/Login"
@@ -20,10 +17,7 @@ const RutasApp = () => {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
-      <Route path="/home" element={<Home />} />
       <Route path="/productos" element={<Productos />} />
-      <Route path="/suplementos" element={<Products />} />
-      <Route path="/tienda" element={<CatalogWithCart />} />
       <Route path="/marcas" element={<Marcas />} />
       <Route path="/ofertas" element={<Ofertas />} />
       <Route path="/carrito" element={<Cart />} />
