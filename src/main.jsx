5e88d8f48c0852461880fdbc10584e5d5ee36e8b
marceloriv <ma.riveracontreras@duocuc.cl>
2025-11-12 13:30:45 -0300

    fix: unificar comillas en la importación de CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import "@styles/index.css"
 import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { CartProvider } from './contexts/CartContext'
+import { CartProvider } from "./contexts/CartContext"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
